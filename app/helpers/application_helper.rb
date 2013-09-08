72b8214ee4a6ba81593d8b5df8f8bbeb115d65fa
Martin Feckie <mfeckie@gmail.com>
2013-09-08 21:59:42 +0800

    Flash message helpers and devise error message formatter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,47 @@
 module ApplicationHelper
+
+  ALERT_DIV = {
+      notice: 'alert alert-info',
+      success: 'alert alert-success',
+      error: 'alert alert-danger',
+      alert: 'alert alert-danger'
+  }
+
+  def flash_messages
+    flash_messages = []
+    flash.each do |type, message|
+      next if message.blank?
+
+      Array(message).each do |msg|
+
+        text = %Q[<div class="alert-block "> <div class="fade in #{ALERT_DIV[type]}">
+          <button class="close" data-dismiss="alert">X</button>
+            <p>#{msg}</p>
+          </div>
+        </div>
+        ]
+        flash_messages << text if msg
+      end
+    end
+    flash_messages.join("\n").html_safe
+  end
+
+  def error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="alert alert-block alert-danger">
+      <h2>#{sentence}</h2>
+      <ul class="list-unstyled">#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
 end
