11d24cdaa68d81284d7e6c0d8292f552ff6dd57a
Martin Feckie <mfeckie@gmail.com>
2013-09-13 00:23:05 +0800

    Fix up migrations names to stop Heroku dying

--- db/migrate/20130910115330_create_urgency_category.rb
+++ db/migrate/20130910115330_create_urgency_category.rb
@@ -1,9 +1,10 @@
-class AddUrgencyCategory < ActiveRecord::Migration
+class CreateUrgencyCategories < ActiveRecord::Migration
   def change
     create_table :urgency_categories do |t|
       t.string :name
       t.integer :number
       t.integer :acceptable_wait_minutes
+      t.integer :acceptable_wait_hours
     end
   end
 end
--- db/migrate/20130911072654_add_acceptable_wait_hours_column_to_urgency_categories.rb
+++
@@ -1,5 +0,0 @@
-class AddAcceptableWaitHoursColumnToUrgencyCategories < ActiveRecord::Migration
-  def change
-    add_column :urgency_categories, :acceptable_wait_hours, :integer
-  end
-end
