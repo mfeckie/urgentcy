2b8950f30ff527e69028438972235de6d7a2c1a1
Martin Feckie <mfeckie@gmail.com>
2013-09-12 17:57:41 +0800

    Let's use inherited resource style to save bucketloads of unecessary code

--- app/controllers/surgical_specialties_controller.rb
+++ app/controllers/surgical_specialties_controller.rb
@@ -1,75 +1,9 @@
-class SurgicalSpecialtiesController < ApplicationController
-  before_action :set_surgical_specialty, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!
-
-  # GET /surgical_specialties
-  # GET /surgical_specialties.json
-  def index
-    @surgical_specialties = SurgicalSpecialty.all
-  end
-
-  # GET /surgical_specialties/1
-  # GET /surgical_specialties/1.json
-  def show
-  end
-
-  # GET /surgical_specialties/new
-  def new
-    @surgical_specialty = SurgicalSpecialty.new
-  end
-
-  # GET /surgical_specialties/1/edit
-  def edit
-  end
-
-  # POST /surgical_specialties
-  # POST /surgical_specialties.json
-  def create
-    @surgical_specialty = SurgicalSpecialty.new(surgical_specialty_params)
-
-    respond_to do |format|
-      if @surgical_specialty.save
-        format.html { redirect_to @surgical_specialty, notice: 'Surgical specialty was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @surgical_specialty }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @surgical_specialty.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /surgical_specialties/1
-  # PATCH/PUT /surgical_specialties/1.json
-  def update
-    respond_to do |format|
-      if @surgical_specialty.update(surgical_specialty_params)
-        format.html { redirect_to @surgical_specialty, notice: 'Surgical specialty was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @surgical_specialty.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /surgical_specialties/1
-  # DELETE /surgical_specialties/1.json
-  def destroy
-    @surgical_specialty.destroy
-    respond_to do |format|
-      format.html { redirect_to surgical_specialties_url }
-      format.json { head :no_content }
-    end
-  end
-
+class SurgicalSpecialtiesController < InheritedResources::Base
+  respond_to :html, :json
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_surgical_specialty
-      @surgical_specialty = SurgicalSpecialty.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def surgical_specialty_params
-      params.require(:surgical_specialty).permit(:name)
-    end
+  def resource_params
+    return [] if request.get?
+    [ params.require(:surgical_specialty).permit! ]
+  end
 end
--- app/controllers/urgency_categories_controller.rb
+++ app/controllers/urgency_categories_controller.rb
@@ -1,64 +1,14 @@
-class UrgencyCategoriesController < ApplicationController
-  before_action :set_urgency_category, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @urgency_categories = UrgencyCategory.all
-  end
-
-  def show
-  end
-
-  def new
-    @urgency_category = UrgencyCategory.new
-  end
-
-  def edit
-
-  end
-
-  def create
-    @urgency_category = UrgencyCategory.new(urgency_category_params)
-
-    respond_to do |format|
-      if @urgency_category.save
-        format.html { redirect_to @urgency_category, notice: "Urgency category successfully created"}
-        format.json { render action: 'show', status: :created, location: @urgency_category}
-      else
-        format.html { render action: :new}
-        format.json { render json: @urgency_category.errors, status: :unprocessable_entity}
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @urgency_category.update(urgency_category_params)
-        format.html { redirect_to @urgency_category, notice: "Urgency category successfully updated"}
-        format.json { head :no_content}
-      else
-        format.html { render action: :edit}
-        format.json { render json: @urgency_category.errors, status: :unprocessable_entity}
-      end
-
-    end
-  end
-
-  def destroy
-    @urgency_category.destroy
-    respond_to do |format|
-      format.html { redirect_to urgency_categories_url}
-      format.json { head :no_content}
-    end
-  end
-
+class UrgencyCategoriesController < InheritedResources::Base
+  respond_to :html, :json
   private
 
-  def set_urgency_category
-    @urgency_category = UrgencyCategory.find(params[:id])
+  def resource_params
+    return [] if request.get?
+    [ params.require(:urgency_category).permit! ]
   end
 
-  def urgency_category_params
-    params.require(:urgency_category).permit(:name, :number, :acceptable_wait_hours, :acceptable_wait_minutes)
+  def build_resource_params
+    [params.require(:urgency_category)]
   end
 
 end
