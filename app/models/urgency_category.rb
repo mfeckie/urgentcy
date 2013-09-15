69b9563ee7d0a75572daf2c71e0e6820fc59a48c
Martin Feckie <mfeckie@gmail.com>
2013-09-15 21:04:02 +0800

    Begin booking form

--- app/models/urgency_category.rb
+++ app/models/urgency_category.rb
@@ -6,4 +6,13 @@ class UrgencyCategory < ActiveRecord::Base
   def time_in_minutes
     (acceptable_wait_hours * 60) + acceptable_wait_minutes
   end
+
+  def time_in_hours
+    time_in_minutes / 60
+  end
+
+  def urgency_in_words
+    "#{name} - #{acceptable_wait_hours} hours #{acceptable_wait_minutes} minutes"
+  end
+
 end
